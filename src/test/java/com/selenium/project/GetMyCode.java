24554919aa8a5b60daed5583c06ba9ed6535ed0a
Kunal7008 <kunal.bhatta513@gmail.com>
2019-05-10 11:11:03 +0530

    first commit

---
+++ src/test/java/com/selenium/project/GetMyCode.java
@@ -0,0 +1,15 @@
+package com.selenium.project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GetMyCode {
+	@Test
+	public void test_01() {
+		System.setProperty("webdriver.chrome.driver", "F:\\downloads\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.navigate().to("https://www.flipkart.com/");
+		System.out.println(driver.getTitle());
+	}
+}
